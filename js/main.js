9205af64c619a481188ec23771333d8203ac53cc
KariK <kari@bighillops.com>
2020-04-15 23:40:26 +0300

    new header

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@ window.onload = () => {
     } 
 }
 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
 installItself = () => {
 
     console.log("installing...");
